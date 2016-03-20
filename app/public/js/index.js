459abb0e3dae7d495ece6546e28c456e32a5a194
Simon White <simonxwhite@gmail.com>
2016-03-20 14:29:06 +0000

    Get athlete data using jsonp

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -7,13 +7,36 @@ $(function() {
     scope: 'public'
   };
 
-  var connectUrl = 'https://www.strava.com/oauth/authorize?' + jQuery.param(params);      
+  var connectUrl = 'https://www.strava.com/oauth/authorize?' + jQuery.param(params);
   $('a#connect').attr('href', connectUrl);
 
   // get token from query string
   var urlParams = getUrlParams();
   console.log('token: ' + urlParams.token);
   $('p#token').text(urlParams.token);
+
+  // get athlete details
+  if (urlParams.token && urlParams.token !== 'error') {
+    var athleteUrl = 'https://www.strava.com/api/v3/athlete';
+    var settings = {
+      url: athleteUrl,
+      data: { access_token: urlParams.token },
+      // headers: { Authorizatdion: 'Bearer ' + urlParams.token },
+      dataType: 'jsonp',
+    };
+
+    $.getJSON(athleteUrl + '?callback=?', settings.data, function(data) {
+       console.log(data);
+    });
+
+    // $.ajax(settings)
+    //   .done(function(data) {
+    //     console.log(data);
+    //   })
+    //   .fail(function(data) {
+    //     console.log(data);
+    //   });
+  }
 
   function getUrlParams() {
     var params = {};
